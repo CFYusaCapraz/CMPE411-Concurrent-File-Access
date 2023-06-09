f2b83b037aff523e96975dcfc4b467465e64eb1c
Yuşa Çapraz <computerhackfusion@gmail.com>
2023-06-09 22:21:11 +0300

    Renamed Child to ClientHandler and done implementing

---
+++ src/com/cyberfreak/file_access/ClientHandler.java
@@ -0,0 +1,57 @@
+package com.cyberfreak.file_access;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private Socket client;
+
+    public ClientHandler(Socket incomingClient) {
+        client = incomingClient;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                PrintWriter out = new PrintWriter(new OutputStreamWriter(client.getOutputStream()))) {
+            while (true) {
+                boolean flag = false;
+                String line = in.readLine();
+                if (line == null) {
+                    break;
+                } else {
+                    String[] fields = line.split(" ");
+                    switch (fields[0]) {
+                        case "display":
+                            StudentManager.displayStudents(out);
+                            break;
+                        case "delete":
+                            StudentManager.deleteStudent(fields[1]);
+                            break;
+                        case "modify":
+                            StudentManager.modifyCGPA(fields[1], fields[2]);
+                            break;
+                        case "add":
+                            StudentManager
+                                    .addStudent(new Student(fields[1], fields[2], fields[3], fields[4], fields[5]));
+                            break;
+                        case "disconnect":
+                            flag = true;
+                            break;
+                    }
+                }
+                if (flag)
+                    break;
+            }
+            in.close();
+            out.close();
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
