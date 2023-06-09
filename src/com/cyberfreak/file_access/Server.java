7893c2a6d80f87cb530278b799162383b8980986
Yuşa Çapraz <computerhackfusion@gmail.com>
2023-06-09 21:43:46 +0300

    Moved loadStudentsFile to Manager class

--- src/com/cyberfreak/file_access/Server.java
+++ src/com/cyberfreak/file_access/Server.java
@@ -1,47 +1,22 @@
 package com.cyberfreak.file_access;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.LinkedList;
 
-public class Server implements Runnable{
+public class Server implements Runnable {
 
     private int PORT;
     private String fileName;
-    private LinkedList<Student> studentList;
 
     public Server(int port, String fileName) {
         this.PORT = port;
         this.fileName = fileName;
     }
 
-    private boolean loadStudentsFile(){
-        File f = new File(this.fileName);
-        try (FileReader reader = new FileReader(f); BufferedReader bReader = new BufferedReader(reader)) {
-            String line;
-            while ((line = bReader.readLine())!= null) {
-                String[] fields = line.split(",");
-                String id = fields[0];
-                String name = fields[1];
-                String cgpa = fields[2];
-                String birthday = fields[3];
-                String gender = fields[4];
-                studentList.add(new Student(id, name, cgpa, birthday, gender));
-            }
-        } catch (IOException e) {
-            System.out.println("Students file could not be found!");
-            return false;
-        }
-        return true;
-    }
-
     @Override
     public void run() {
-        if(loadStudentsFile()){
+        if (StudentManager.loadStudentsFile(this.fileName)) {
             try {
                 ServerSocket socket = new ServerSocket(PORT);
                 while (true) {
@@ -53,5 +28,5 @@ public class Server implements Runnable{
             }
         }
     }
-    
+
 }
